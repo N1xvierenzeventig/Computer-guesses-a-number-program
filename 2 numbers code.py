6ca5fb7e020dd0986377cd585e550c116ad0a960
N1xvierenzeventig <111287243+N1xvierenzeventig@users.noreply.github.com>
2023-04-09 15:51:36 +0200

    Update 2 numbers code.py

--- 2 numbers code.py
+++ 2 numbers code.py
@@ -2,26 +2,33 @@ import random
 
 def computer(x):
     global Times
+    global answer
+    global guess
     low = 1
     high = x
     feedback = ""
     answer = int(input("Input your number which is right: "))
-    Times = 0
+    Times = 1
+    Available_guesses = 5
     guess = random.randint(1, x)
     print(guess)
-    while guess != answer:
+    while guess != answer and Available_guesses != 1:
         if answer > guess:
             feedback = "l"
             low = guess + 1
+            Available_guesses -= 1
             guess = random.randint(low,high)
         elif answer < guess:
             feedback = "h"
             high = guess - 1
+            Available_guesses -= 1
             guess = random.randint(low,high)
-        Times += 1
+            Times += 1
         print(guess)
 
 
-computer(10)
-print(f"You didn't get that for {Times} times")
-
+computer(199)
+if answer == guess:
+    print(f"You won and used your chance for {Times} times, congrats!!!")
+elif answer != guess:
+    print(f"You've lost the game and used your chance for {Times + 1}")
